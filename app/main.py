2cd6956de158a4a986815ab983451670e0c7e915
JAINRAS PETER P <jainras2004@gmail.com>
2026-02-13 20:55:21 +0530

    Update Port number #JP

--- app/main.py
+++ app/main.py
@@ -32,4 +32,4 @@ app.include_router(router)
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app.main:app", host="0.0.0.0", port=3000, reload=True)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=10000, reload=True)
