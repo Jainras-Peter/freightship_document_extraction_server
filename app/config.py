9015f9adb0332a341d70916c27710b7b85b300f3
JAINRAS PETER P <jainras2004@gmail.com>
2026-04-22 18:22:32 +0530

    host

--- app/config.py
+++ app/config.py
@@ -19,6 +19,9 @@ class Settings:
     DB_NAME = os.getenv("DB_NAME", "fs-extraction_server-db")
     CACHE_ENABLED = os.getenv("CACHE_ENABLED", "False").lower() == "true"
 
+    # Debug
+    DEBUG = os.getenv("DEBUG", "False").lower() == "true"
+
     # Server
     PORT = int(os.getenv("PORT", 10000))
 
